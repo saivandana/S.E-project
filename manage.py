8ae53c7908eb4b542b4d5e25a60f648ed3fd2185
NSreelasya <167364967+NSreelasya@users.noreply.github.com>
2024-05-02 15:32:59 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run all the administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shopping.settings')
     try:
         from django.core.management import execute_from_command_line
